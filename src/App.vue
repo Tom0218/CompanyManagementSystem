21368b53ad02b3a05c8668563bc3d586aec6290c
HsiehYuTeng <mark29737148mark@gmail.com>
2023-12-26 00:49:43 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+import { RouterLink, RouterView } from 'vue-router'
+export default{
+  components:{
+    RouterView,
+  },
+}
+</script>
+
+<template>
+  <div class="body">
+    <RouterView />
+  </div>
+
+</template>
+
+<style lang="scss" scoped>
+.body{
+  height: auto;
+  width: 100vw;
+  background-color: rgb(9, 38, 53);
+}
+</style>
