e772913bd2361df45a13e20870bcc66d23824e6e
HsiehYuTeng <mark29737148mark@gmail.com>
2023-12-31 16:45:11 +0800

    create dev

--- src/components/Staff/StaffCreate.vue
+++ src/components/Staff/StaffCreate.vue
@@ -2,6 +2,33 @@
 <template>
     <div class="StaffCreateBody">
         <h1>StaffCreate</h1>
+        <label for="">編號 :</label>
+        <input type="text">
+        <br/>
+        <label for="">部門 :</label>
+        <input type="text">
+        <br/>
+        <label for="">姓名 :</label>
+        <input type="text">
+        <br/>
+        <label for="">密碼 :</label>
+        <input type="text">
+        <br/>
+        <label for="">信箱 :</label>
+        <input type="text">
+        <br/>
+        <label for="">啟用 :</label>
+        <input type="text">
+        <br/>
+        <label for="">職位 :</label>
+        <input type="text">
+        <br/>
+        <label for="">生日 :</label>
+        <input type="date">
+        <br/>
+        <label for="">到職日期 :</label>
+        <input type="date">
+        <br/>
     </div>
 </template>
 <style lang="scss" scoped>
