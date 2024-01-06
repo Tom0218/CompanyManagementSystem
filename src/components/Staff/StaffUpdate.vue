5b416a45fa704004cd12b8ddfb2febd74ced305a
HsiehYuTeng <mark29737148mark@gmail.com>
2024-01-06 16:11:18 +0800

    updateAndforgotPwdOK

--- src/components/Staff/StaffUpdate.vue
+++ src/components/Staff/StaffUpdate.vue
@@ -1,12 +1,193 @@
-<script></script>
+<script>
+import {mapState, mapActions} from 'pinia';
+import userInfo  from'../../stores/userInfo';
+
+export default{
+    data(){
+        return{
+            page:"",
+            userData:[],
+            employee:"",
+            id :"",
+            department:"",
+            selectedDepartment:"",
+            name:"",
+            pwd:"",
+            email:"",
+            active:false,
+            jobPosition:"",
+            birthDate:"",
+            arrivalDate:""
+        }
+    },
+    mounted(){
+        this.fetchUser()
+    },
+    computed:{
+    // 使用 mapState 将 state 映射到组件的计算属性
+    //參數:  資料庫,要用的 state & getters
+    ...mapState(userInfo ,["user"]),
+    },
+    methods:{
+        ...mapActions(userInfo ,["setUser",'getUser']),
+        async fetchUser() {
+        // 触发从后端获取用户数据的操作
+        this.userData = this.getUser();
+        console.log('Fetched userData:', this.userData);
+
+
+        },
+        goUpdateActive(){
+            this.page = "updateActive"
+        },
+        updateActive(action){
+            //判斷選擇啟用或停用
+            if(action == "active"){
+                this.active = true;
+            }else{
+                this.active = false;
+            }
+            // 定义请求参数
+            const executorId = this.userData.id;
+            const employeeId = this.id;
+            const isActive = this.active;
+
+            // 构建 API 地址
+            const apiUrl = `http://localhost:8080/api/attendance/employee/updateActive?executorId=${executorId}&employeeId=${employeeId}&isActive=${isActive}`;
+
+            // 发起 fetch 请求
+            fetch(apiUrl, {
+            method: 'POST', // 或者 'GET'，根据你的 API 需求
+            headers: {
+                'Content-Type': 'application/json', // 根据实际需要设置请求头
+            },
+            })
+            .then(response => {
+                if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+                return response.json();
+            })
+            .then(data => {
+                // 处理返回的数据
+                console.log(data);
+                alert(data.rtnCode)
+            })
+            .catch(error => {
+                // 处理请求错误
+                console.error('Fetch error:', error);
+            });
+        },
+        search(){
+            console.log(this.id)
+            const id = this.id;
+            const name = this.name;
+            const department = this.selectedDepartment;
+            const apiUrl = 'http://localhost:8080/api/employee/search';
+            
+            // Construct the URL with query parameters
+            const urlWithParams = `${apiUrl}?id=${id}&name=${name}&department=${department}`;
+
+            // Make a GET request
+            fetch(urlWithParams, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json', // You can adjust the content type if needed
+                    "Content-Type": "application/json",
+                    "Access-Control-Allow-Origin": "*"
+                },
+                // Add other options if needed
+            })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);
+                this.employee = data.employeeList;
+            })
+            .catch(error => {
+                console.error('Fetch Error:', error);
+            });
+        },
+
+    }
+}
+</script>
 <template>
-    <div class="StaffUpdateBody">
-        <h1>StaffUpdate</h1>
+    <div class="StaffUpdateBody" >
+        <button @click="goUpdateActive">帳號啟用/帳號停用</button>
+        <div v-if="this.page == 'updateActive'">
+            <label for="">員工編號 : </label>
+            <input type="text" v-model="id">
+            <button @click="search">搜尋</button>
+            <button @click="updateActive('active')">啟用</button>
+            <button @click="updateActive('deactivate')">停用</button>
+            <table>
+                <tr>
+                    <th>員工編號</th>
+                    <th>部門</th>
+                    <th>職位</th>
+                    <th>姓名</th>
+                    <th>信箱</th>
+                    <th>生日</th>
+                    <th>到職日期</th>
+                    <th>離職日期</th>
+                    <th>離職原因</th>
+                    <th>帳號狀態</th>
+                    <th>特休</th>
+                    <th>病假</th>
+                </tr>
+                <tr v-for="employee,index  in this.employee" :key="index">
+                    <td :key="index">{{ employee.id }}</td>
+                    <td :key="index">{{ employee.department }}</td>
+                    <td :key="index">{{ employee.jobPosition }}</td>
+                    <td :key="index">{{ employee.name }}</td>
+                    <td :key="index">{{ employee.email }}</td>
+                    <td :key="index" class="date">{{ employee.birthDate }}</td>
+                    <td :key="index" class="date">{{ employee.arrivalDate }}</td>
+                    <td :key="index">{{ employee.resignationDate }}</td>
+                    <td :key="index">{{ employee.quitReason }}</td>
+                    <td :key="index">{{ employee.active }}</td>
+                    <td :key="index">{{ employee.annualLeave }}</td>
+                    <td :key="index">{{ employee.sickLeave }}</td>
+                </tr>
+            </table>
+        </div>
     </div>
+    
 </template>
 <style lang="scss" scoped>
+tr{
+    text-align: center;
+}
+td{
+    width: 5%;
+    border: 1px black solid;
+    text-align: left;
+    font-weight: bold;
+    padding: 0 1%;
+    // margin: 0;
+}
+th{
+    width: 5%;
+    border: 1px black solid;
+    font-weight: bold;
+}
+table{
+    width: 100vw;
+    background-color: whitesmoke;
+    overflow: auto;
+}
+label{
+    color: whitesmoke;
+}
+h1{
+    color: whitesmoke;
+}
 .StaffUpdateBody{
-    height: 100vh;
     width: 100vw;
     background-color: #092635;
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,8 +7,13 @@ export default{
     return{
       page:"未登入",
       id:"",
+      employeeNum: "",
       pwd:"",
+      newPwd:"",
       info:[],
+      email:"",
+      userInput:"",
+      authCode:"",
     }
   },
   computed:{
@@ -20,6 +25,105 @@ export default{
   methods:{
     ...mapActions(userInfo ,["setUser",'getUser']),
 
+    changePwd(){
+      const req = {
+      id: this.employeeNum, // replace with the actual employee ID
+      auth_code: this.authCode, // replace with the actual authentication code
+      new_password: this.newPwd // replace with the actual new password
+    };
+    console.log(this.employeeNum+this.authCode+this.newPwd)
+    const apiUrl = "http://localhost:8080/api/attendance/employee/change_password_by_auth_code";
+
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(req),
+    };
+
+    // Making the fetch request
+    fetch(apiUrl, requestOptions)
+      .then(response => response.json())
+      .then(data => {
+        // Handle the response data
+        console.log(data);
+      })
+      .catch(error => {
+        // Handle errors
+        console.error('Error:', error);
+      });
+
+    },
+    //傳送驗證碼(id/email)
+    SendVerificationCode(){
+      //判斷是email還是員工編號
+      if(this.userInput.includes('@')){
+        this.email = this.userInput
+      } else {
+        this.employeeNum = this.userInput
+      }
+      
+      const requestData = {
+        id: this.employeeNum,
+        email: this.email
+      };
+
+      // 发起POST请求
+      fetch('http://localhost:8080/api/attendance/employee/forgot_password', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+          // 如果有其他必要的头部信息，可以在这里添加
+        },
+        body: JSON.stringify(requestData)
+      })
+      .then(response => response.json())
+      .then(data => {
+        // 处理从服务器返回的数据（data）
+        console.log(data);
+        this.employeeNum = "";
+        this.email = ""
+        
+      })
+      .catch(error => {
+        // 处理请求过程中的错误
+        console.error('Error:', error);
+      });
+    },
+    search(){
+      const id = this.id;
+      const name = this.name;
+      const department = this.selectedDepartment;
+      const apiUrl = 'http://localhost:8080/api/employee/search';
+      
+      // Construct the URL with query parameters
+      const urlWithParams = `${apiUrl}?id=${id}&name=${name}&department=${department}`;
+
+      // Make a GET request
+      fetch(urlWithParams, {
+          method: 'GET',
+          headers: {
+              'Content-Type': 'application/json', // You can adjust the content type if needed
+              "Content-Type": "application/json",
+              "Access-Control-Allow-Origin": "*"
+          },
+          // Add other options if needed
+      })
+      .then(response => {
+          if (!response.ok) {
+              throw new Error(`HTTP error! Status: ${response.status}`);
+          }
+          return response.json();
+      })
+      .then(data => {
+          this.employeeList = data.employeeList;
+          console.log('employeeList:', this.employeeList);
+      })
+      .catch(error => {
+          console.error('Fetch Error:', error);
+      });
+    },
     logout() {
             var url = "http://localhost:8080/api/attendance/logout";
             var data = {};
@@ -49,31 +153,30 @@ export default{
 				"id":this.id,
 				"password":this.pwd
 			};
-
-            fetch(url, {
-            method: "POST", // or 'PUT'
-            credentials:'include',
-            body: JSON.stringify(data), // data can be `string` or {object}!
-            headers: new Headers({
-                "Content-Type": "application/json",
-                "Access-Control-Allow-Origin": "*",
-                //api session不同要加
-                "Access-Control-Allow-Credentials":true  
-            }),
-            })
-            .then((res) => res.json())
-            .catch((error) => console.error("Error:", error))
-            .then((response) =>{ 
-              alert(response.rtnCode)
-                console.log("Login:", response,)   
-                if(response.rtnCode == "SUCCESSFUL"){
-                  this.info = response;
-                  this.setUser(response.employee);
-                  this.$router.push("/SelectFuntionPage");
-                  return
-                }
-                return
-            });
+        fetch(url, {
+        method: "POST", // or 'PUT'
+        credentials:'include',
+        body: JSON.stringify(data), // data can be `string` or {object}!
+        headers: new Headers({
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+            //api session不同要加
+            "Access-Control-Allow-Credentials":true  
+        }),
+        })
+        .then((res) => res.json())
+        .catch((error) => console.error("Error:", error))
+        .then((response) =>{ 
+          alert(response.rtnCode)
+            console.log("Login:", response,)   
+            if(response.rtnCode == "SUCCESSFUL"){
+              this.info = response;
+              this.setUser(response.employee);
+              this.$router.push("/SelectFuntionPage");
+              return
+            }
+            return
+        });
     }
   }
 
@@ -82,20 +185,75 @@ export default{
 <template>
 <div class="homeBody">
   <div class="body">
-  <label>帳號:</label>
-  <input type="text" id="account" v-model="id">
-  <br/>
-  <label>密碼:</label>
-  <input type="text" id="pwd" v-model="pwd">
-  <br/>
-  <button @click="login">登入</button>
-  <!-- <button @click="register">註冊</button> -->
-  <button @click="forgotPwd">忘記密碼</button>
-</div>
+    <label>帳號:</label>
+    <input type="text" id="account" v-model="id">
+    <br/>
+    <label>密碼:</label>
+    <input type="text" id="pwd" v-model="pwd">
+    <br/>
+    <button @click="login">登入</button>
+    <!-- <button @click="register">註冊</button> -->
+
+    <!-- Button trigger modal -->
+    <button @click="forgotPwd"  data-bs-toggle="modal" data-bs-target="#exampleModal">忘記密碼(未完成)</button>
+
+  <!-- Modal -->
+    <div class="modal fade " id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">忘記密碼</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <div class="inputArea">
+              <div>
+                <label for="" class="lable">請輸入員工編號或電子信箱:</label>
+                <input type="text" v-model="userInput"  id="idAndEmailInputBox">
+                <button @click="SendVerificationCode()">送出驗證碼</button>
+              </div>
+              <br/>
+              <label class="lable">員工編號 :</label>
+              <input type="text" v-model="employeeNum">
+              <br/>
+              <label class="lable">驗證碼 :</label>
+              <input type="text" v-model="authCode">
+              <br/>
+              <label class="lable">新密碼 :</label>
+              <input type="text" v-model="newPwd">
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">關閉</button>
+            <button type="button" class="btn btn-primary" @click="changePwd">完成</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>  
 </template>
 
 <style lang="scss" scoped>
+
+#idAndEmailInputBox{
+  width: 74%;
+}
+.inputArea{
+  width: 70%;
+  margin: 0 15%;
+  display: flex;
+  flex-direction: column;
+  // align-items: center;
+}
+.modal-content{
+  width: 115%;
+}
+
+.lable{
+  color: black;
+}
+
 button{
   font-weight: bold;
 }
